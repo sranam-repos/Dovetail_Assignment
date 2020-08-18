66394e5575389c9148cd9200f517ade6944f1e37
Sranam1991 <ranamswathi@gmail.com>
2020-08-19 03:05:06 +1200

    Replaced Javascript custom countdown function with react-countdown component

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const loader = () => (<div className="Loader">Loading..</div>);
+export default loader;
--- src/containers/BarcodeComponent/BarcodeComponent.js
+++ src/containers/BarcodeComponent/BarcodeComponent.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import Countdown from 'react-countdown';
 import Barcode from 'react-barcode';
 import './BarcodeComponent.css';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import Loader from '../../components/UI/Loader/Loader';
 
 const BarcodeComponent = (props) => {
   //Creating and initializing states for barcode and countdown time
   const [countDownTimer, setCountDownTimer] = useState('');
   const [barcode, setBarcode] = useState('');
+  const [showLoader, setShowLoader] = useState(false);
 
   //Introducing side effects after render cycle to avoid blocking of component rendering. Passing empty array to avoid unnecessary re-rendering.
   useEffect(() => {
@@ -15,62 +18,44 @@ const BarcodeComponent = (props) => {
 
   //Function to call generate-barcode API using javascript Fetch API
   const fetchBarcodeHandler = () => {
+    setBarcode('');
+    setCountDownTimer('');
+    setShowLoader(true);
     fetch('https://jet-gull-7204.twil.io/generate-barcode').then(response => 
       response.json()
     ).then(responseData => {
       setBarcode(responseData.barcode.toString());
       //Coverting UTC ISO Date string to localized Date string
-      let timerCountdown = new Date(new Date(responseData.expiresAt).getTime());
+      const timerCountdown = responseData.expiresAt.toString();
       setCountDownTimer(timerCountdown);
+      setShowLoader(false);
     }).catch((err) => {
       //Any error logic goes here
       console.log(err);
     });
   };
-
-  //Function to calculate and return remaining time for expiry
-  const getExpiryTime = () => {
-    const timeRemaining = new Date(countDownTimer) - new Date();
-    let timeLeft = {};
-    if(timeRemaining > 0) {
-      timeLeft = {
-        minutes: Math.floor((timeRemaining / 1000 / 60) % 60),
-        seconds: Math.floor((timeRemaining / 1000) % 60)
-      }
-    }
-    return timeLeft;
-  }
-  //State to manage expiry time
-  const [expiryTime, setExpiryTime] = useState('');
-
-  useEffect(() => {
-    setTimeout(() => {
-      setExpiryTime(getExpiryTime());
-    },1000);
-    //on unmount clear timeout
-    return () => clearTimeout(countDownTimer);
-  });
   
-  const fetchButtonComponent = [];
-  Object.keys(expiryTime).forEach((key) => {
-    if (!expiryTime[key]) {
-      return;
+  const rendererHandler = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      return <button className='ButtonStyle' onClick={fetchBarcodeHandler}>Fetch Barcode</button>;
+    } else {
+      return (
+        <p className="TimerWrapper">
+          {minutes}:{seconds}
+        </p>
+      );
     }
-    fetchButtonComponent.push(
-      <p key={expiryTime[key]} className="TimerWrapper">
-        {expiryTime['minutes']}:{expiryTime['seconds']}{" "}
-      </p>
-    );
-  });
-  
+  };
+
   return (
     <Auxiliary>
       <h1>Barcode</h1>
-      {barcode.length ? <Barcode value={barcode}/> : ''}
-      <div className="ComponentWrapper">
-        {fetchButtonComponent.length ? fetchButtonComponent : <button className="ButtonStyle" onClick={fetchBarcodeHandler}>Fetch Barcode</button>}
-      </div>
-
+      {showLoader? <Loader></Loader> : ''}
+      {barcode.length ? (
+          <div key="timer" className = "ComponentWrapper">
+            <Barcode key="barcode" value={barcode}/>
+            <Countdown date={new Date(countDownTimer)} renderer={rendererHandler}/>
+          </div>) : ''}
     </Auxiliary>
   );
 };

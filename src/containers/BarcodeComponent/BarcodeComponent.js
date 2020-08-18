6f94f790c1e030daeb10ac5658a9f1ef91122fa9
Sranam1991 <ranamswathi@gmail.com>
2020-08-18 19:00:32 +1200

    Rendered Barcode from react-barcode

--- src/containers/BarcodeComponent/BarcodeComponent.js
+++ src/containers/BarcodeComponent/BarcodeComponent.js
@@ -6,7 +6,7 @@ import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 const barcodeComponent = (props) => (
     <Auxiliary>
         <h1>Barcode</h1>
-        <div>Placeholder for barcode</div>
+        <Barcode value="Bar code number placeholder"/>
         <p>Placeholder for timer</p>
         <button>Fetch Barcode</button>
     </Auxiliary>
